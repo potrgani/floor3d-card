ffd050094abb787b43228fc7a141e5054934b008
potrgani <drluka123456@gmail.com>
2024-04-14 17:44:27 +0200

    Update floor3d-card.ts

--- src/floor3d-card.ts
+++ src/floor3d-card.ts
@@ -1293,7 +1293,7 @@ export class Floor3dCard extends LitElement {
 
     // create and initialize renderer
 
-    this._renderer = new THREE.WebGLRenderer({logarithmicDepthBuffer: true, alpha: true });
+    this._renderer = new THREE.WebGLRenderer({antialias: true, logarithmicDepthBuffer: true, alpha: true });
     this._maxtextureimage = this._renderer.capabilities.maxTextures;
     console.log('Max Texture Image Units: ' + this._maxtextureimage);
     console.log('Max Texture Image Units: number of lights casting shadow should be less than the above number');
@@ -1469,7 +1469,7 @@ export class Floor3dCard extends LitElement {
 
       this._controls = new OrbitControls(this._camera, this._renderer.domElement);
 
-      //this._renderer.setPixelRatio(window.devicePixelRatio);
+      this._renderer.setPixelRatio(window.devicePixelRatio);
 
       this._controls.maxPolarAngle = (0.85 * Math.PI) / 2;
       this._controls.addEventListener('change', this._changeListener);
